ce3ec3bfecce4d01dd80dd8979bb92e3f3c31fda
Raphael Mello <raphaelpmros@hotmail.com>
2024-02-08 15:09:03 -0300

    Testando se a conexão com express e o db vai funcionar

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// const express = require("express");
-// const app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
 const mysql = require('mysql2');
+
 
 const con = mysql.createConnection({
   host: 'mysql-db',
@@ -13,9 +15,20 @@ const con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE IF NOT EXISTS movies (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), year INT, score DOUBLE, rating VARCHAR(3))";
+  var sql = "CREATE TABLE IF NOT EXISTS games (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), year INT, score DOUBLE, rating VARCHAR(30))";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
   });
 });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/dog', (req, res) => {
+  res.send("WOLF")
+})
+
+app.listen(3000, () => {
+  console.log("listening on port 3000")
+})
